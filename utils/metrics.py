9271f0e94fbc8a1452098c63b5e5c6711cfb280b
jimmy133719 <39381342+jimmy133719@users.noreply.github.com>
2019-07-04 16:50:50 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,4 @@
 # This file contains basic metrics used for evaluation
-# Author: Stefan Kahl, 2018, Chemnitz University of Technology
 
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import label_ranking_average_precision_score
